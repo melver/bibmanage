cbe59b70b1b7afbdd2ac00c2420ef769e72ad5c0
Marco Elver <me@marcoelver.com>
2012-03-09 05:53:35 +0000

    [sync] Allowing lists for paths

--- lib/python/bibman/commands/sync.py
+++ lib/python/bibman/commands/sync.py
@@ -47,16 +47,19 @@ class SyncCommand:
             self.bibfmt_efs.append(bi)
 
     def walk_path(self):
-        if not os.path.isdir(self.args.path):
-            logging.error("Could not find directory: {}".format(self.args.path))
-            return
+        for path in self.args.paths:
+            if not os.path.isdir(path):
+                logging.error("Could not find directory: {}".format(path))
+                continue
 
-        for root, dirs, files in os.walk(self.args.path):
-            for f in files:
-                fullpath = os.path.abspath(os.path.join(root,f))
-                if fullpath.startswith(os.environ['HOME']):
-                    fullpath = fullpath.replace(os.environ['HOME'], "~", 1)
-                yield fullpath
+            for root, dirs, files in os.walk(path):
+                for f in files:
+                    fullpath = os.path.abspath(os.path.join(root,f))
+                    if fullpath.startswith(os.environ['HOME']):
+                        fullpath = fullpath.replace(os.environ['HOME'], "~", 1)
+
+                    if fullpath.split(".")[-1] in self.args.extlist:
+                        yield fullpath
 
     def __call__(self):
         for path in self.walk_path():
@@ -132,8 +135,8 @@ def main(args):
         bibfile = open(args.bibfile, 'r+')
 
         excludefiles = []
-        if args.exclude is not None:
-            for filename in args.exclude.split(","):
+        if args.excludes is not None:
+            for filename in args.excludes:
                 excludefiles.append(open(filename, "r"))
     except Exception as e:
         logging.critical("Could not open file: {}".format(e))
@@ -149,17 +152,17 @@ def main(args):
 
 def register_args(parser):
     parser.add_argument("-p", "--path", metavar="PATH", type=str,
-            dest="path", required=True,
-            help="Path to scan and synchronise BIBFILE with.")
+            dest="paths", nargs="+", required=True,
+            help="Paths to scan and synchronise BIBFILE with.")
     parser.add_argument("--extlist", type=str,
-            dest="extlist", default="pdf",
-            help="Command separated list of file-extensions to consider for sync. [Default:pdf]")
+            dest="extlist", default="pdf", nargs="+",
+            help="File-extensions to consider for sync. [Default:pdf]")
     parser.add_argument("-a", "--append", action='store_true',
             dest="append", default=False,
             help="Append to BIBFILE instead of printing to stdout.")
     parser.add_argument("-e", "--exclude", metavar="EXCLUDE", type=str,
-            dest="exclude", default=None,
-            help="Comma separated list of BibTeX files to exclude new entries from.")
+            dest="excludes", default=None, nargs="+",
+            help="BibTeX files to exclude new entries from.")
     parser.add_argument("--nohash", action="store_false",
             dest="hash", default=True,
             help="Do not generate MD5 sums and check duplicates.")
